cc100365b9d826d979c43476be77334cff72c470
Ajinath-Game <gameajinath4@gmail.com>
2025-11-10 19:54:48 +0530

    add jwt token

--- server/controller/auth.control.js
+++ server/controller/auth.control.js
@@ -46,37 +46,41 @@ const postSignup = async (req, res) => {
 }
 
 const postLogin = async (req, res) => {
-    let { email, password } = req.body;
+    try {
+        let { email, password } = req.body;
 
-    if (!email || !password) {
-        return responder(res, 406, null, "to login email & password is required", false);
+        if (!email || !password) {
+            return responder(res, 406, null, "to login email & password is required", false);
+        }
+
+        let findedUser = await User.findOne({ email: email });
+
+        if (!findedUser) {
+            return responder(res, 404, null, "user not found", false);
+        }
+
+        let isPassMatch = await bcrypt.compare(password, findedUser.password);
+
+        if (!isPassMatch) {
+            return responder(res, 404, null, "invalid password", false);
+
+        }
+
+        let token = jwt.sign({
+            id: findedUser._id,
+            name: findedUser.name,
+            email: findedUser.email
+        }, process.env.JWT_SECRET)
+
+        req.session.token = token;
+
+        return responder(res, 200, null, "login successfully", true);
+    } catch (error) {
+        return responder(res, error.status || 500, null, error.message, false)
     }
-
-    let findedUser = await User.findOne({ email: email });
-
-    if (!findedUser) {
-        return responder(res, 404, null, "user not found", false);
-    }
-
-    let isPassMatch = await bcrypt.compare(password, findedUser.password);
-
-    if (!isPassMatch) {
-        return responder(res, 404, null, "invalid password", false);
-
-    }
-      
-    let token = jwt.sign({
-        id:findedUser._id,
-        name:findedUser.name,
-        email:findedUser.email
-    },process.env.JWT_SECRET)
-
-    req.session.token = token;
-
-    return responder(res, 200, null, "login successfully", true);
-    
+ 
 
 }
 
 
-export { postSignup , postLogin }
+export { postSignup, postLogin }
